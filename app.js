3e7df65d89cedb8d8d0df99fd307487a1fc56e88
ijankauskas <ignacio.jankauskas60@gmail.com>
2022-05-31 19:04:40 -0300

    se cambia el header y se agrega el login

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path')
 
 app.use(express.static('public'));
 
+app.get('/login',(req, res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
 app.get('/register',(req, res) =>{
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
